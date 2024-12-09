58c01bd43bb257a85bc2facfd14da2cf0c82df3a
CarlosMabrey <75705516+CarlosMabrey@users.noreply.github.com>
2024-12-09 12:46:24 -0700

    Add files via upload
    updated intro/conclusion/data preparation sections, added plt_util,py file to make the plots more straightforward to generate.

---
+++ plt_util.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def create_plots(audio, time, frequency, activation, confidence):
+    # Calculate the average activation
+    average_activation = activation.mean(axis=1)
+
+    # Plotting all the graphs together in a grid
+    fig, axs = plt.subplots(2, 2, figsize=(20, 16))
+
+    # Plot Core Frequency
+    axs[0, 0].plot(time, frequency, color='purple', linewidth=3)
+    axs[0, 0].set_xlabel('Time (s)', fontsize=14)
+    axs[0, 0].set_ylabel('Frequency (Hz)', fontsize=14)
+    axs[0, 0].set_title('Core Frequency Over Time', fontsize=16)
+    axs[0, 0].grid(True)
+
+    # Plot Max Amplitude
+    axs[0, 1].plot(audio, color='purple', linewidth=3)
+    axs[0, 1].set_xlabel('Sample Rate Index (length)', fontsize=14)
+    axs[0, 1].set_ylabel('Amplitude', fontsize=14)
+    axs[0, 1].set_title('Amplitude Over Time', fontsize=16)
+    axs[0, 1].grid(True)
+
+    # Plot Average Activation
+    axs[1, 0].plot(time, average_activation, color='purple', linewidth=3)
+    axs[1, 0].set_xlabel('Time (s)', fontsize=14)
+    axs[1, 0].set_ylabel('Average Activation', fontsize=14)
+    axs[1, 0].set_title('Average Activation Over Time', fontsize=16)
+    axs[1, 0].grid(True)
+
+    # Plot Confidence
+    axs[1, 1].plot(time, confidence, color='purple', linewidth=3)
+    axs[1, 1].set_xlabel('Time (s)', fontsize=14)
+    axs[1, 1].set_ylabel('Confidence', fontsize=14)
+    axs[1, 1].set_title('Confidence Over Time', fontsize=16)
+    axs[1, 1].grid(True)
+
+    plt.tight_layout()
+    plt.savefig('plots/transparent/all_plots_grid.png', transparent=True)
+    plt.show()
+
+
+def violin_plot(data):
+    _, axes = plt.subplots(2, 2, figsize=(12, 10))
+
+    for ax, column in zip(axes.flat, data.columns[:-1]):  # Exclude 'label'
+        sns.violinplot(data=data, x='label', y=column, ax=ax, palette='Set2', hue='label', legend=False)
+        ax.set_title(f'Distribution Of {column.replace("_", " ").title()} By Label', fontsize=12)
+        ax.set_xlabel('Label', fontsize=10)
+        ax.set_ylabel(column.replace("_", " ").title(), fontsize=10)
+
+    plt.tight_layout()
+    plt.show()
+
+def correlation_matrix(data):
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data.drop('label', axis=1).corr(), annot=True, cmap='coolwarm', fmt='.2f')
+    plt.title('Correlation Matrix', fontsize=16)
+    plt.show()
